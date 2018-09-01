bb0f60bdc21e6f4518a83205744be3f0482c573b
Miguel Gonzalez <miguel.gonzalez.ordonez@gmail.com>
2018-08-31 19:26:38 -0500

    Test

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AppmenuComponent } from './appmenu/appmenu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppmenuComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
